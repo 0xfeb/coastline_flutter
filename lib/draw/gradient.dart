b5d91f364b35088493b33085df484bca3cc3ad15
0xfeb <wangyuanou@foxmail.com>
2021-03-26 12:52:09 +0800

    修改gradient 接口

--- lib/draw/gradient.dart
+++ lib/draw/gradient.dart
@@ -12,49 +12,47 @@ enum Direction {
   toBottomRight,
 }
 
-extension GradientExtra on LinearGradient {
-  /// >>> 生成渐变图形 >>>
-  static LinearGradient fromList(
-      {@required List<Color> colors,
-      List<double> stops,
-      Direction direction = Direction.toRight}) {
-    AlignmentGeometry begin;
-    AlignmentGeometry end;
-    switch (direction) {
-      case Direction.toDown:
-        begin = Alignment.topLeft;
-        end = Alignment.bottomLeft;
-        break;
-      case Direction.toUp:
-        begin = Alignment.bottomLeft;
-        end = Alignment.topLeft;
-        break;
-      case Direction.toRight:
-        begin = Alignment.topLeft;
-        end = Alignment.topRight;
-        break;
-      case Direction.toLeft:
-        begin = Alignment.topRight;
-        end = Alignment.topLeft;
-        break;
-      case Direction.toTopLeft:
-        begin = Alignment.bottomRight;
-        end = Alignment.topLeft;
-        break;
-      case Direction.toTopRight:
-        begin = Alignment.bottomLeft;
-        end = Alignment.topRight;
-        break;
-      case Direction.toBottomLeft:
-        begin = Alignment.topRight;
-        end = Alignment.bottomLeft;
-        break;
-      case Direction.toBottomRight:
-        begin = Alignment.topLeft;
-        end = Alignment.bottomRight;
-        break;
-    }
-
-    return LinearGradient(colors: colors, stops: stops, begin: begin, end: end);
+/// >>> 生成渐变 >>>
+LinearGradient gradient(
+    {@required List<Color> colors,
+    List<double> stops,
+    Direction direction = Direction.toRight}) {
+  AlignmentGeometry begin;
+  AlignmentGeometry end;
+  switch (direction) {
+    case Direction.toDown:
+      begin = Alignment.topLeft;
+      end = Alignment.bottomLeft;
+      break;
+    case Direction.toUp:
+      begin = Alignment.bottomLeft;
+      end = Alignment.topLeft;
+      break;
+    case Direction.toRight:
+      begin = Alignment.topLeft;
+      end = Alignment.topRight;
+      break;
+    case Direction.toLeft:
+      begin = Alignment.topRight;
+      end = Alignment.topLeft;
+      break;
+    case Direction.toTopLeft:
+      begin = Alignment.bottomRight;
+      end = Alignment.topLeft;
+      break;
+    case Direction.toTopRight:
+      begin = Alignment.bottomLeft;
+      end = Alignment.topRight;
+      break;
+    case Direction.toBottomLeft:
+      begin = Alignment.topRight;
+      end = Alignment.bottomLeft;
+      break;
+    case Direction.toBottomRight:
+      begin = Alignment.topLeft;
+      end = Alignment.bottomRight;
+      break;
   }
+
+  return LinearGradient(colors: colors, stops: stops, begin: begin, end: end);
 }
