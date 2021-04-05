a7a3acb573c3661ce60040e93d62350a9b393ecc
0xfeb <wangyuanou@foxmail.com>
2021-04-06 06:31:47 +0800

    添加中文阿拉伯混合数字提取

--- lib/structure/number_parser.dart
+++ lib/structure/number_parser.dart
@@ -18,25 +18,45 @@ extension NumberParser on String {
     return n;
   }
 
+  int get _multiChineseNumber {
+    int result = 0;
+    for (int i = 0; i < this.length; ++i) {
+      result = result * 10 + this[i]._singleChineseNumber;
+    }
+    return result;
+  }
+
   /// >>> 根据中文文本获得具体的数值 >>>
   int get chineseNumber {
     final Pair<String, String> wp = this.apartBy('万');
-    print('wp -> $wp');
     final String w = wp.a ?? '零';
     final Pair<String, String> qp = wp.b.apartBy('千');
-    print('qp -> $qp');
     final String q = qp.a ?? '零';
     final Pair<String, String> bp = qp.b.apartBy('百');
-    print('bp -> $bp');
     final String b = bp.a ?? '零';
     final Pair<String, String> sp = bp.b.apartBy('十');
-    print('sp -> $sp');
     final String s = sp.a ?? '零';
     final String g = sp.b ?? '零';
     return w._singleChineseNumber * 10000 +
         q._singleChineseNumber * 1000 +
         b._singleChineseNumber * 100 +
         s._singleChineseNumber * 10 +
-        g._singleChineseNumber;
+        g._multiChineseNumber;
+  }
+
+  /// >>> 中文和阿拉伯混合文本得到的数值 >>>
+  int get mixChineseNumber {
+    String mix = this.replaceAll('1', "一");
+    mix = mix.replaceAll('2', "二");
+    mix = mix.replaceAll('3', "三");
+    mix = mix.replaceAll('4', "四");
+    mix = mix.replaceAll('5', "五");
+    mix = mix.replaceAll('6', "六");
+    mix = mix.replaceAll('7', "七");
+    mix = mix.replaceAll('8', "八");
+    mix = mix.replaceAll('9', "九");
+    mix = mix.replaceAll('0', "零");
+
+    return mix.chineseNumber;
   }
 }
