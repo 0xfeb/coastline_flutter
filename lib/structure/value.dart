59cd295cd2e02e17c158227ae13c2498865ce161
0xfeb <wangyuanou@foxmail.com>
2021-04-06 06:42:51 +0800

    把空判断从类内转移到函数

--- lib/structure/value.dart
+++ lib/structure/value.dart
@@ -1,15 +1,12 @@
-/// >>> 数据判断类 >>>
-class Value {
-  /// >>> 判断一个数值是否为空 >>>
-  static bool isEmpty(dynamic l) {
-    if (l == null || l == Null) {
-      return true;
-    }
-
-    if (l is List || l is String || l is Map || l is Set) {
-      return l.isEmpty;
-    }
-
-    return false;
+/// >>> 判断一个数值是否为空 >>>
+bool isEmpty(dynamic l) {
+  if (l == null || l == Null) {
+    return true;
   }
+
+  if (l is List || l is String || l is Map || l is Set) {
+    return l.isEmpty;
+  }
+
+  return false;
 }
