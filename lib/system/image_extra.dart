e3b9475b5f1e805f8bc0f8e91b5911dcf59c106d
0xfeb <wangyuanou@foxmail.com>
2021-04-06 12:41:34 +0800

    将几个图片文件加载功能从扩展转移到函数

--- lib/system/image_extra.dart
+++ lib/system/image_extra.dart
@@ -4,20 +4,34 @@ import 'package:flutter/widgets.dart';
 import 'file_path.dart';
 import 'package:uuid/uuid.dart';
 
+/// >>> 文件图像, 从Storage ID获取图像 >>>
+FileImage loadStorageImage(String storageId) {
+  String destPath = FilePath().imageFilename(storageId);
+  print('load image from -> $destPath');
+  return FileImage(File(destPath));
+}
+
+/// >>> 文件图像, 从Storage ID获取图像, 同步 >>>
+Future<FileImage> loadStorageImageAsync(String storageId) async {
+  await FilePath().setup();
+  return loadStorageImage(storageId);
+}
+
+/// >>> 拷贝一个文件到Storage ID >>>
+String loadPathCopyToStorageImage(String sourcePath) {
+  String uuid = Uuid().v4();
+  String destFile = FilePath().imageFilename(uuid);
+  File(sourcePath).copySync(destFile);
+  return uuid;
+}
+
+/// >>> 拷贝一个文件到Storage ID, 同步 >>>
+Future<String> loadPathCopyToStorageImageAsync(String sourcePath) async {
+  await FilePath().setup();
+  return loadPathCopyToStorageImage(sourcePath);
+}
+
 extension ImageExtra on FileImage {
-  /// >>> 文件图像, 从Storage ID获取图像 >>>
-  static FileImage fromStorageId(String storageId) {
-    String destPath = FilePath().imageFilename(storageId);
-    print('load image from -> $destPath');
-    return FileImage(File(destPath));
-  }
-
-  /// >>> 文件图像, 从Storage ID获取图像, 同步 >>>
-  static Future<FileImage> fromStorageIdAsync(String storageId) async {
-    await FilePath().setup();
-    return fromStorageId(storageId);
-  }
-
   /// >>> 存储图像, 自动返回一个Storage ID >>>
   String save() {
     String uuid = Uuid().v4();
@@ -33,19 +47,5 @@ extension ImageExtra on FileImage {
   Future<String> saveAsync() async {
     await FilePath().setup();
     return save();
-  }
-
-  /// >>> 拷贝一个文件到Storage ID >>>
-  static copyToStorage(String sourcePath) {
-    String uuid = Uuid().v4();
-    String destFile = FilePath().imageFilename(uuid);
-    File(sourcePath).copySync(destFile);
-    return uuid;
-  }
-
-  /// >>> 拷贝一个文件到Storage ID, 同步 >>>
-  static copyToStorageAsync(String sourcePath) async {
-    await FilePath().setup();
-    copyToStorage(sourcePath);
   }
 }
