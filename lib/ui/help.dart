619a53be31d319e22c98c6137f2d2a1a63e55a5a
0xfeb <wangyuanou@foxmail.com>
2021-04-10 09:33:36 +0800

    分离help_item

---
+++ lib/ui/help.dart
@@ -0,0 +1,12 @@
+import '../draw/line.dart';
+import 'package:flutter/cupertino.dart';
+
+
+
+class Help extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
---
+++ lib/ui/help_item.dart
@@ -0,0 +1,9 @@
+import 'dart:ui';
+
+class HelpItem {
+  final String text;
+  final Offset arrowPoint;
+  final Offset endPoint;
+
+  HelpItem({this.text, this.arrowPoint, this.endPoint});
+}
