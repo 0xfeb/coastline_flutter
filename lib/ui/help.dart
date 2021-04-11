f802f622420d6c819f05ddfd934dcdf29ac7026c
0xfeb <wangyuanou@foxmail.com>
2021-04-11 21:58:17 +0800

    完成帮助控件的内容

--- lib/ui/help.dart
+++ lib/ui/help.dart
@@ -1,13 +1,31 @@
+import 'package:coastline/draw/canvas_view.dart';
 import 'package:flutter/cupertino.dart';
 
 import './help_item.dart';
 
 class Help extends StatelessWidget {
-  List<HelpItem> items;
+  final List<HelpItem> items;
+
+  const Help({Key key, this.items}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    final screenSize = MediaQuery.of(context).size;
+    return Expanded(
+        child: Stack(
+      children: items.map((e) {
+        HelpItem item = HelpItem(
+          color: e.color,
+          text: e.text,
+          arrowPoint: Offset(e.arrowPoint.dx * screenSize.width,
+              e.arrowPoint.dy * screenSize.height),
+          endPoint: Offset(e.endPoint.dx * screenSize.width,
+              e.endPoint.dy * screenSize.height),
+        );
+        return Positioned(child: CanvasView(onPaint: (canvas, size) {
+          canvas.drawHelpItem(item);
+        }));
+      }).toList(),
+    ));
   }
 }
