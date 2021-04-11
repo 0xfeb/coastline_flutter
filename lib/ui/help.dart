50e6aa5a71a3c5c437beb2a99090cb4a6aa6437d
0xfeb <wangyuanou@foxmail.com>
2021-04-11 19:15:51 +0800

    调整呢

--- lib/ui/help.dart
+++ lib/ui/help.dart
@@ -1,9 +1,10 @@
-import '../draw/line.dart';
 import 'package:flutter/cupertino.dart';
 
-
+import './help_item.dart';
 
 class Help extends StatelessWidget {
+  List<HelpItem> items;
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
