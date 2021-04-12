8d8464e69103732274a95ad7097b3d4083c866ca
0xfeb <wangyuanou@foxmail.com>
2021-04-12 22:42:41 +0800

    把帮助的功能从类变成函数

--- lib/ui/help.dart
+++ lib/ui/help.dart
@@ -1,36 +1,52 @@
 import 'package:coastline/draw/canvas_view.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 import './help_item.dart';
+import './widget_extra.dart';
 
-class Help extends StatelessWidget {
-  final List<HelpItem> items;
+showHelp({BuildContext context, List<HelpItem> items}) {
+  final screenSize = MediaQuery.of(context).size;
+  List<Widget> children = [
+    Positioned(
+      child: CircleAvatar(
+        child: Icon(Icons.close),
+      ).onTap(() {
+        Navigator.of(context).pop();
+      }),
+      width: 30,
+      height: 30,
+      left: 20,
+      top: 40,
+    ),
+  ];
+  for (HelpItem e in items) {
+    HelpItem item = HelpItem(
+      color: e.color,
+      text: e.text,
+      arrowPoint: Offset(e.arrowPoint.dx * screenSize.width,
+          e.arrowPoint.dy * screenSize.height),
+      endPoint: Offset(
+          e.endPoint.dx * screenSize.width, e.endPoint.dy * screenSize.height),
+    );
+    children.add(Positioned(
+        child: CanvasView(
+            size: screenSize,
+            onPaint: (canvas, size) {
+              canvas.drawHelpItem(item);
+            })));
+  }
 
-  const Help({Key key, this.items}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    final screenSize = MediaQuery.of(context).size;
-    return SizedBox(
+  showDialog(
+    context: context,
+    builder: (c) {
+      return SizedBox(
         width: screenSize.width,
         height: screenSize.height,
         child: Stack(
-          children: items.map((e) {
-            HelpItem item = HelpItem(
-              color: e.color,
-              text: e.text,
-              arrowPoint: Offset(e.arrowPoint.dx * screenSize.width,
-                  e.arrowPoint.dy * screenSize.height),
-              endPoint: Offset(e.endPoint.dx * screenSize.width,
-                  e.endPoint.dy * screenSize.height),
-            );
-            return Positioned(
-                child: CanvasView(
-                    size: screenSize,
-                    onPaint: (canvas, size) {
-                      canvas.drawHelpItem(item);
-                    }));
-          }).toList(),
-        ));
-  }
+          children: children,
+        ),
+      );
+    },
+  );
 }
