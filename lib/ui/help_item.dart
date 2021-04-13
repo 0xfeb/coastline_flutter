d88768c19689fe230ee688714acee96938d311e7
0xfeb <wangyuanou@foxmail.com>
2021-04-13 14:58:50 +0800

    修正helpitem中的文本位置

--- lib/ui/help_item.dart
+++ lib/ui/help_item.dart
@@ -54,7 +54,7 @@ extension CanvasHelp on Canvas {
     pg.layout(ui.ParagraphConstraints(width: 200));
     print('item ${item.text} ${pg.width}');
     final textOffset = upArrow
-        ? Offset(max(10, item.endPoint.dx - 10), item.endPoint.dy - 20)
+        ? Offset(max(10, item.endPoint.dx - 10), item.endPoint.dy - 25)
         : Offset(max(10, item.endPoint.dx - 10), item.endPoint.dy);
     print('text offset $textOffset');
     this.drawParagraph(pg, textOffset);
