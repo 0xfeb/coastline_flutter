3fb745e037549517d0ac8f7a81700f20ffd2d686
WangYuanOu <wangyuanou@foxmail.com>
2022-01-18 22:12:30 +0800

    暴露接口

--- lib/coastline.dart
+++ lib/coastline.dart
@@ -1,4 +1,29 @@
 library coastline2;
 
-export './ui/widget_extra.dart';
+export './draw/color_extra.dart';
+export './draw/gradient.dart';
+export './draw/resource.dart';
 export './draw/shape_border.dart';
+
+export './structure/combo_data.dart';
+export './structure/list_extra.dart';
+export './structure/map_extra.dart';
+export './structure/number_extra.dart';
+export './structure/number_parser.dart';
+export './structure/string_extra.dart';
+export './structure/value.dart';
+
+export './system/image_extra.dart';
+export './system/path.dart';
+
+export './ui/context_extra.dart';
+export './ui/control.dart';
+export './ui/grid_list.dart';
+export './ui/mini_control.dart';
+export './ui/number_field.dart';
+export './ui/pad.dart';
+export './ui/rich_text.dart';
+export './ui/stroke_text.dart';
+export './ui/tag_list.dart';
+export './ui/text_display.dart';
+export './ui/widget_extra.dart';
