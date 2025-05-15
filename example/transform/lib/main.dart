aae266cf7b900206d0b9cd99aa857f9f8e4edc7e
danielwyo <wangyuanou@foxmail.com>
2025-05-15 19:33:44 +0800

    feat(example): 添加 Coastline 依赖并进行页面展示
    - 在 pubspec.yaml 中添加 Coastline 依赖
    - 在 main.dart 中导入 Coastline 并使用动画示例
    - 更新 Linux、macOS 和 Windows 平台的插件注册文件
    - 更新 pubspec.lock 文件以包含新的依赖项

--- example/transform/lib/main.dart
+++ example/transform/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:coastline/coastline.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -56,6 +57,8 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
 
+  Alignment _animationAlignment = Alignment.topLeft;
+
   void _incrementCounter() {
     setState(() {
       // This call to setState tells the Flutter framework that something has
@@ -105,10 +108,9 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
+            Container(color: Colors.red, height: 20, width: 20)
+                .alignmentAnimate(animateValueForAlignment: _animationAlignment)
+                .box(width: 100, height: 100, color: Colors.blue),
           ],
         ),
       ),
--- example/transform/macos/Flutter/GeneratedPluginRegistrant.swift
+++ example/transform/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,16 @@
 import FlutterMacOS
 import Foundation
 
+import ffmpeg_kit_flutter_new
+import file_selector_macos
+import package_info_plus
+import path_provider_foundation
+import sqflite_darwin
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FFmpegKitFlutterPlugin.register(with: registry.registrar(forPlugin: "FFmpegKitFlutterPlugin"))
+  FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
+  FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
 }
